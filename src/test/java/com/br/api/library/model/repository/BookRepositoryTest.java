87f3e7108850f52748ef6427a007b7a632f3a835
Lucas Galvao <lucas.engcomp@outlook.com>
2020-07-22 20:56:13 -0300

    Organizando ambiente para teste de integração.

---
+++ src/test/java/com/br/api/library/model/repository/BookRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.br.api.library.model.repository;
+
+import com.br.api.library.api.model.repositoy.BookRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest //cria uma instancia no bd em memoria para executar os testes somente em tempo de execução -> H2 DB
+public class BookRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager; //criar um cenário para teste
+
+    @Autowired
+    BookRepository repository;
+
+    @Test
+    @DisplayName("Deve retornar verdadeiro quando existir um livro na base com o ISBN informado.")
+    public void returnTtrueWhenIsbnExisits() {
+        //cenário
+        String isbn = "123";
+
+        //execução
+        boolean exists = repository.existsByIsbn(isbn);
+
+        //verificação
+        assertThat(exists).isTrue();
+    }
+
+}
